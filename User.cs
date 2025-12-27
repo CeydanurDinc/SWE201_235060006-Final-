037105b57cea8a21cbae836145816daf50a60cf0
Ceydanur Dinç <235060006@toros.edu.tr>
2025-12-27 14:42:41 +0300

    User.cs

--- User.cs
+++ User.cs
@@ -1,11 +1,28 @@
-﻿public class User
+using System.Collections.Generic;
+
+public class User
 {
-    public int UserId { get; set; }
-    public string Name { get; set; }
+    public int UserId { get; }
+    public string Name { get; }
+
+    public List<Movie> WatchedMovies { get; } = new List<Movie>();
 
     public User(int userId, string name)
     {
         UserId = userId;
         Name = name;
+    }
+
+    public void WatchMovie(Movie movie, int rating)
+    {
+        movie.SetRating(rating);
+
+        if (!WatchedMovies.Contains(movie))
+            WatchedMovies.Add(movie);
+    }
+
+    public bool HasWatched(Movie movie)
+    {
+        return WatchedMovies.Contains(movie);
     }
 }
